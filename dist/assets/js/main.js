394388a12c5d2b68efc14e250044af35f3a9c043
Gopalakrishnan008 <gopalakrishnan638078@.com>
2024-02-08 19:44:52 +0530

    fix:responsive on md devices

--- dist/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-const nav = document.querySelector('.nav-toggle');
-const lines = document.querySelector('.main-items')
-nav.addEventListener('click',()=>{
-   lines.classList.toggle('display-block'); 
-});
