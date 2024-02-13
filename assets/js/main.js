56a531b9d431686742003678ef635c3e66533095
Gopalakrishnan008 <gopalakrishnan638078@.com>
2024-02-13 09:02:34 +0530

    delete the js file

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const btnEl = document.getElementById('mobileNavBtn');
-const listEl = document.getElementById('mobileNav');
-
-btnEl.addEventListener('click',()=>{
-    listEl.classList.toggle('toggle')
-})
