d21f047932994568a1b4ab8873b59e31c0079731
Gopalakrishnan008 <gopalakrishnan638078@.com>
2024-03-08 12:01:24 +0530

    feat:sending gmail

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const btnEl = document.getElementById('mobileNavBtn');
-    const listEl = document.getElementById('mobileNav');
-    btnEl.addEventListener('click', () => {
-      listEl.classList.toggle('toggle')
-    });
-    const sendmsgEl =document.getElementById("sendmsg");
-    sendmsgEl.addEventListener('click',() => {
-      alert("Message Send Succesfully");
-    });
